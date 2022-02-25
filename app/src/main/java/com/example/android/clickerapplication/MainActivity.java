c045d4202711e5b7b1d97d477bb5ea609f4f1fd4
Funcrafter4 <funcrafter123456@gmail.com>
2022-02-26 02:27:01 +0600

    Fix Buttons on Main

--- app/src/main/java/com/example/android/clickerapplication/MainActivity.java
+++ app/src/main/java/com/example/android/clickerapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,10 +26,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register = (TextView) findViewById(R.id.register);
+        register = (Button) findViewById(R.id.register);
         register.setOnClickListener(this);
 
-        login = (TextView) findViewById(R.id.login);
+        login = (Button) findViewById(R.id.login);
         login.setOnClickListener(this);
 
         editTextEmail = (EditText) findViewById(R.id.Email);
